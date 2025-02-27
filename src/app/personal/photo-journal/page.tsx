dadde64d356da5e040222b16b88e8daca51cea98
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2025-02-27 09:50:16 +0000

    adding placeholder images

--- src/app/personal/photo-journal/page.tsx
+++ src/app/personal/photo-journal/page.tsx
@@ -1,18 +1,80 @@
+import Image  from 'next/image'
+
+type JournalEntryData = {
+    photos: Array<{ id: string, author: string, url: string }>,
+    description: string,
+    createdDate: string,
+    location: string,
+}
+
+// some dummy data to test the journal entry component
+const journalEntries: JournalEntryData[] = [
+    {
+        photos: [{ id: '1', author: 'Author 1', url: 'https://picsum.photos/id/1/200/300' }],
+        description: 'This is the first journal entry.',
+        createdDate: '2023-01-01',
+        location: 'Location 1',
+    },
+    {
+        photos: [{ id: '2', author: 'Author 2', url: 'https://picsum.photos/id/2/200/300' }],
+        description: 'This is the second journal entry.',
+        createdDate: '2023-02-01',
+        location: 'Location 2',
+    },
+    {
+        photos: [{ id: '3', author: 'Author 3', url: 'https://picsum.photos/id/3/200/300' }],
+        description: 'This is the third journal entry.',
+        createdDate: '2023-03-01',
+        location: 'Location 3',
+    },
+    {
+        photos: [{ id: '4', author: 'Author 4', url: 'https://picsum.photos/id/4/200/300' }],
+        description: 'This is the fourth journal entry.',
+        createdDate: '2023-04-01',
+        location: 'Location 4',
+    },
+]
+
 export default function JournalPage() {
+    const JournalEntryComps = journalEntries.map((entry) => {
+        return (
+            <JournalEntry key={entry.createdDate} />
+        )
+    })
     return(
-        <div className="flex flex-col items-center justify-center w-full h-full">
+        <>
+        <Header />
+        <main className="flex flex-col items-center justify-center w-full">
+            {JournalEntryComps}
+        </main>
+        </>
+    )
+}
+
+function Header() {
+    return (
+        <header className="flex flex-row items-center justify-center m-2">
             <h1>Journal Page</h1>
-            <p className="journal-page__description">This is a journal page.</p>
-            <JournalEntry />
-        </div>
+        </header>
     )
 }
 
 function JournalEntry() {
     return(
-        <div className="flex flex-row items-center justify-center w-full h-full">
+        <div className="flex flex-col items-center justify-center ">
+            <Image src="https://picsum.photos/200/300" alt="random image" height={300} width={200} />
             <h2>Journal Entry</h2>
             <p>This is a journal entry.</p>
         </div>
     )
 }
+// // Built-in next js functionality for static site generation
+// // This function will run at build time
+// export const getStaticProps: GetStaticProps = async () => {
+//     const data = await fetch('https://picsum.photos/v2/list?limit=10')
+//     const photos = await data.json()
+//     return {
+//         props: { photos }, // will be passed to the page component as props
+//         revalidate: 60 * 60 * 10, // 10 mins in seconds
+//     }
+// }
