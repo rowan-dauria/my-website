fc45ce29d5da92a4588c73686ef566942c4a50c7
Rowan d'Auria <56480877+rowan-dauria@users.noreply.github.com>
2026-02-12 17:43:13 +0000

    remove whitespace

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="h-full w-full">
-      <body 
+      <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased h-full w-full`}
       >
         {children}
